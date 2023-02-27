2a4519b52797c7665a73737fd4c6a84e392e57ea
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2023-02-27 20:03:50 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 """
-    Queries Reddit's API to find all hot posts listed for a given subreddit.
+    Queries Reddit's API to print the number of occurences specified words in
+    all the hot posts for a given subreddit.
 """
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """ Recursively find all the hot posts for a given Reddit subreddit.
+def count_words(subreddit, word_list, after=None, word_counter={}):
+    """ Recursively find all the hot posts for a given Reddit subreddit and
+        tally the number of occurences of words specified in word_list which
+        occur in the titles.
 
     Args:
         subreddit (str): The name of the subreddit that will be used to query
         the API.
-        host_list (list of str): List containing titles of hot posts of the
-        subreddit.
+        word_list (list of str): List containing words to look for in the
+        titles of hot posts in a subreddit.
         after (str): ID of the last hot post in the returned result. Used to
         get the next 100 hot posts after this ID.
-
-    Returns:
-        The list containing the titles of all hot articles of a given
-        subreddit.
-        None if the subreddit does not exist.
     """
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
     (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
@@ -38,11 +36,23 @@ def recurse(subreddit, hot_list=[], after=None):
             post_dict = post.get('data', {})
             title = post_dict.get('title')
             if title is not None:
-                hot_list.append(title)
+                for word in title.lower().split():
+                    for i in range(len(word_list)):
+                        if word_list[i].lower() == word:
+                            word_counter[word_list[i]] = \
+                                word_counter.get(word_list[i], 0) + 1
+
         after = data_dict.get('after')
         if after is None:
-            return hot_list
+            if word_counter != {}:
+                val_dict = {}
+                for key in word_counter:
+                    if val_dict.get(word_counter[key]) is None:
+                        val_dict[word_counter[key]] = [key]
+                    else:
+                        val_dict[word_counter[key]].append(key)
+                for key in sorted(val_dict.keys(), reverse=True):
+                    for word in sorted(val_dict[key]):
+                        print('{}: {}'.format(word, key))
         else:
-            return (recurse(subreddit, hot_list, after))
-    else:
-        return None
+            count_words(subreddit, word_list, after, word_counter)
